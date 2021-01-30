5627a0fe373a88ed76ac9b34aa523caed4a66e3a
aloeseva <45678913579@mail.ru>
2021-01-31 00:45:05 +0300

    first entry

---
+++ src/main/java/webcourses/webcourse/WebCourseApplication.java
@@ -0,0 +1,13 @@
+package webcourses.webcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webcourses/webcourse/WebCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package webcourses.webcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
